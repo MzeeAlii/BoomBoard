f9081be720ec2284cefecbb9717202261345d560
Ali Suleiman <allysuley@gmail.com>
2017-05-20 21:57:34 +0300

    Add Bookmarks item on navigation and BookmarksActivity

---
+++ app/src/main/java/creationsofali/boomboard/activities/BookmarksActivity.java
@@ -0,0 +1,49 @@
+package creationsofali.boomboard.activities;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+import creationsofali.boomboard.R;
+import creationsofali.boomboard.appfonts.MyCustomAppFont;
+
+public class BookmarksActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookmarks);
+
+        toolbar = (Toolbar) findViewById(R.id.mToolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_back);
+        }
+
+        // set my custom app font
+        View rootView = findViewById(android.R.id.content);
+        new MyCustomAppFont(getApplicationContext(), rootView);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
