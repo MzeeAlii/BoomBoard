bee238971dd7a531e88d66f81d3200a76299f206
Ali Suleiman <allysuley@gmail.com>
2017-07-20 08:53:32 +0300

    Make a Toast when File manager can't be found
    When the user is trying to open the directory (BoomBoard folder) on storage, the Toast is shown when the app couldn't find a File Manager.

--- app/src/main/java/creationsofali/boomboard/helpers/DirectoriesHelper.java
+++ app/src/main/java/creationsofali/boomboard/helpers/DirectoriesHelper.java
@@ -3,6 +3,7 @@ package creationsofali.boomboard.helpers;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.widget.Toast;
 
 /**
  * Created by ali on 7/19/17.
@@ -18,5 +19,9 @@ public class DirectoriesHelper {
 
         if (openFolderIntent.resolveActivityInfo(context.getPackageManager(), 0) != null)
             context.startActivity(openFolderIntent);
+        else
+            Toast.makeText(context,
+                    "File Manager unavailable or not configured properly!",
+                    Toast.LENGTH_LONG).show();
     }
 }
